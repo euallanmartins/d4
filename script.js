62b46e3968ce65c35f619702b3c36ca625f58f55
allan <allan.riberio@gmail.com>
2022-01-27 11:25:58 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+function exibirMensagem(){
+    console.log("oi")
+}
+
+exibirMensagem()
+
+function exibirNome(nome){
+    console.log(`seu nome é ${nome}`)
+}
+
+exibirNome("allan")
+
+function Dados(nome, numero, estiloMusical){
+    console.log(`Nome: ${nome}. Número: ${numero}. Estilo Musical: ${estiloMusical}`)
+}
+
+Dados("allan", 3, "rap")
+
+function filmeMusica(filme, musica){
+    console.log(`Filme: ${filme}. Música: ${musica}`)
+}
+
+filmeMusica("Midsommar - O Mal Não Espera a Noite", "djonga, solto")
+
+function triplo(num){
+    console.log(num*3)
+}
+
+triplo(3)
